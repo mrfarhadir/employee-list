5d1e45739ffd4e2c0cb22ad2ef2d92fb8459ceab
Farhad Mehryari <info@mrfarhad.ir>
2022-03-23 12:09:25 +0430

    update home page e2e test case for filter section

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Test Home Page", () => {
+  it("Should render filter section title", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("h3", "Filter by name and office");
   });
 });
